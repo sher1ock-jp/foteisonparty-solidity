65b991791a26041d58c56af89efecc04e9988cdb
takayukiono1001 <takaaaaadayo@gmail.com>
2023-06-09 12:08:56 +0900

    struct initial

--- contract/contracts/FoteisonGame.sol
+++ contract/contracts/FoteisonGame.sol
@@ -2,17 +2,46 @@
 // fetch the square data from the contract
 // write the new square data to the contract and emit an event
 // when a user move to a new square, if the square has the events that move the user to another square or change the user's balance, then do it and emit an event(renew front-end data)
-// if user move to a square that force the user to do the designated transaction, prohibit the user to roll the dice until the transaction is done
-
+// if user move to a square that force the user to do the designated transaction, prohibit the user to roll the dice until the transaction is don
 
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "hardhat/console.sol";
 
 contract FoteisonGame {
-  constructor() {
-    console.log("THIS IS MY GAME CONTRACT.");
+  //struct Square { creater's address, creater's icon, square's coordinates（X,Y), square's description, square's id,backend's square's id, target's square's id, nft's address, nft's chain, Quest's description, Quest's contract address, user's address, userBalanceChange; }
+  struct Square{
+    address createrAddress;
+    string createrIcon;
+    int squareCoordinateX;
+    int squareCoordinateY;
+    string description;
+    // uint squareId;
+    uint backendSquareId;
+    uint targetSquareId;
+    address nftAddress;
+    string nftChain;
+    string questDescription;
+    address questContractAddress;
+    uint userBalanceChange;
+    address userAddress;
   }
+
+  struct User{
+    int userCoordinateX;
+    int userCoordinateY;
+    uint userBalance;
+    bool userQuestStatus;
+  }
+
+  // array of squares
+  Square[] public squares;
+
+  // using Counters for Counters.Counter;
+  // Counters.Counter private _squareId;
+
+
 }
--- contract/test/run.js
+++ contract/test/run.js
@@ -1,8 +1,9 @@
+// npx hardhat test
 // run.js
 const main = async () => {
-    const gameContractFactory = await hre.ethers.getContractFactory("FoterGame");
+    const gameContractFactory = await hre.ethers.getContractFactory("FoteisonGame");
     const gameContract = await gameContractFactory.deploy();
-    const fotetisonGame = await gameContract.deployed();
+    const foteisonGame = await gameContract.deployed();
   
     console.log("Contract deployed to:", foteisonGame.address);
   };
