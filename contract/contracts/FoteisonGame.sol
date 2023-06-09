73f78d132ba68d2e521a29f8a283c69654506e3c
takayukiono1001 <takaaaaadayo@gmail.com>
2023-06-09 17:25:10 +0900

    contract initial

--- contract/contracts/FoteisonGame.sol
+++ contract/contracts/FoteisonGame.sol
@@ -1,4 +1,4 @@
-// write the profile data to the contract
+// write the user data to the contract
 // fetch the square data from the contract
 // write the new square data to the contract and emit an event
 // when a user move to a new square, if the square has the events that move the user to another square or change the user's balance, then do it and emit an event(renew front-end data)
@@ -12,36 +12,61 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "hardhat/console.sol";
 
 contract FoteisonGame {
-  //struct Square { creater's address, creater's icon, square's coordinates（X,Y), square's description, square's id,backend's square's id, target's square's id, nft's address, nft's chain, Quest's description, Quest's contract address, user's address, userBalanceChange; }
   struct Square{
     address createrAddress;
     string createrIcon;
-    int squareCoordinateX;
-    int squareCoordinateY;
     string description;
-    // uint squareId;
     uint backendSquareId;
-    uint targetSquareId;
-    address nftAddress;
-    string nftChain;
+    // uint targetSquareId;
+    string nftURL;
     string questDescription;
     address questContractAddress;
     uint userBalanceChange;
-    address userAddress;
+    uint[] adjacentSquareIds;
   }
 
+  // retrive the sqare data from the squareId from the front-end
+  mapping(uint => Square) public squareIdToSquare;
+
   struct User{
-    int userCoordinateX;
-    int userCoordinateY;
+    uint squareId;
     uint userBalance;
     bool userQuestStatus;
   }
 
+  // retrive the user data from the userAddress from the front-end
+  mapping(address => User) public users;
+
   // array of squares
   Square[] public squares;
 
-  // using Counters for Counters.Counter;
-  // Counters.Counter private _squareId;
+  function createSquare(
+    // struct
+  string memory _createrIcon,
+    string memory _description,
+    uint _backendSquareId,
+    // uint _targetSquareId,
+    string memory _nftURL,
+    string memory _questDescription,
+    address _questContractAddress,
+    uint _userBalanceChange,
+    // array of adjacent square ids
+  ) Square memory newSquare = Square(
+      msg.sender,
+      _createrIcon,
+      _description,
+      _backendSquareId,
+      // _targetSquareId,
+     _nftURL,
+      _questDescription,
+      _questContractAddress,
+      _userBalanceChange,
+      // address(0),
+      new uint[](0) 
+    );
 
+    squareIdToSquare[newSquareId] = newSquare;
+    squares.push(newSquare);
 
-}
+    return newSquareId;
+  }
